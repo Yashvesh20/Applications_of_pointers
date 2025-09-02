51ab7a04a2fe101ac0df9f44cc67785eeb5f90c6
Yashvesh20 <yashvesh20@gmail.com>
2025-09-02 14:32:22 +0530

    Create Call_by_Reference.cpp

---
+++ Call_by_Reference.cpp
@@ -0,0 +1,17 @@
+Yashvesh Singh 24070123138
+#include <iostream>
+using namespace std;
+void swap(int *x,int *y){
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+int main() {
+    int a=20,b=22;
+    swap(&a,&b);
+    cout<<a<<endl<<b;
+}
+output:
+22
+20
